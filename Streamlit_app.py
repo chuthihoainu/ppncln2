a6f742ca8d09261f78320b9558a09c0f663f0bbd
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 05:10:26 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from markdown import markdown
 st.image("download.jpg")
-st.write("VPANDAS xin chào ạ")
+import streamlit as st
+
+# Định dạng văn bản
 text = "VPANDAS xin chào ngày cuối deadline"
 
 # Hiển thị văn bản
-
-markdown(f"""<p style="color:green;font-weight:bold;">{text}</p>""")
+st.write(f"""<p style="color:green;font-weight:bold;">{text}</p>""", unsafe_allow_html=True)

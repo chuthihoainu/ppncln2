7441e7a3908d1a018708808c6b67cb8a461a5d24
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 04:26:18 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 import streamlit as st
-my_code ='
+my_code ='https://github.com/chuthihoainu/ppncln2/commit/a0fac088057c909ed3f46a18ee3b93df490c6ddd'
 # Nhập mã Python từ file my_code.py
 from my_code import *
 

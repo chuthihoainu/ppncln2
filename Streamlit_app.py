d1e384f46eb64f1ba8072e3d82cbbdb2dce56f35
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 04:22:50 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.title("hello")
+
+
+# Mở file Python
+with open("Main_RFM_#.ipynb", "r") as f:
+    content = f.read()
+
+# Hiển thị nội dung file
+st.code(content, language="python")

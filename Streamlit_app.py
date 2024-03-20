17551a7c443f053b3787a8b6f0c8dc6634b87218
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 05:11:47 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from markdown import markdown
 st.image("download.jpg")
-import streamlit as st
+
 
 # Định dạng văn bản
+
 text = "VPANDAS xin chào ngày cuối deadline"
 
 # Hiển thị văn bản

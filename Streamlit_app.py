a2619b825b86d6f612a87061f96a0ecdb3816f5c
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 04:47:59 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,17 +1,27 @@
-
+import streamlit as st
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-# Load the Jupyter Notebook file
-notebook_file = 'Main_RFM_3.ipynb'
-with open(notebook_file) as f:
-    nb = nbformat.read(f, as_version=4)
+# Load the Jupyter Notebook file from GitHub
+url = 'https://github.com/chuthihoainu/ppncln2/master/Main_RFM_3.ipynb'
+
+# Read the Jupyter Notebook content
+with st.spinner('Loading the notebook...'):
+    nb = nbformat.reads(requests.get(url).text, as_version=4)
 
 # Execute the notebook
 ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
 ep.preprocess(nb, {'metadata': {'path': ''}})
 
-# Save the executed notebook as a Python script
-output_file = 'executed_notebook.py'
-with open(output_file, 'w') as f:
-    f.write(nbformat.writes(nb))
+# Display the output cells in Streamlit
+st.markdown("### Displaying Jupyter Notebook Results")
+for cell in nb.cells:
+    if cell.cell_type == 'code' and cell.outputs:
+        st.write('```python')
+        st.code(cell.source)
+        st.write('```')
+        for output in cell.outputs:
+            if output.output_type == 'execute_result':
+                st.write(output.data['text/plain'])
+            elif output.output_type == 'display_data':
+                st.image(output.data['image/png'])

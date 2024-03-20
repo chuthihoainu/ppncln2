ec656bfb36774633cc56abeadc138e202c703c8d
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 04:38:08 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # Load the Jupyter Notebook file from GitHub
-url = 'https://raw.githubusercontent.com/your_username/your_repository/main/your_notebook.ipynb'
+url = 'https://github.com/chuthihoainu/ppncln2/blob/master/Main_RFM_3.ipynb'
 
 # Display the content of the Jupyter Notebook file
 st.markdown(f"### Displaying Jupyter Notebook from GitHub\n[Click here to view the notebook]({url})")

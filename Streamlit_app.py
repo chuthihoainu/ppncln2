7b29d0ba5a018fe7c5f42da3bce4014c148bd4ba
chuthihoainu <151080575+chuthihoainu@users.noreply.github.com>
2024-03-21 04:39:22 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -7,4 +7,4 @@ url = 'https://github.com/chuthihoainu/ppncln2/blob/master/Main_RFM_3.ipynb'
 
 # Display the content of the Jupyter Notebook file
 st.markdown(f"### Displaying Jupyter Notebook from GitHub\n[Click here to view the notebook]({url})")
-streamlit run your_script.py
+streamlit run app.py
